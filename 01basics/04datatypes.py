76737289026f9b3f5b8689b63073fec4fba6cd02
akshita garg <akshitagarg275@gmail.com>
2022-09-26 08:59:09 +0530

    day 3 : datatypes, typeconversion

---
+++ 01basics/04datatypes.py
@@ -0,0 +1,113 @@
+# TODO : primitive
+
+# # TODO: int
+# age = 14
+# print("age is :",age)
+# print(type(age))
+
+# TODO: float
+# price = 25.34
+# print("price is :",price)
+# print(type(price))
+
+
+# TODO: complex (real num +/- imaginary num)
+# 5 + 3j
+# c = 5 + 3j
+# print("c is :",c)
+# print(type(c))
+
+# TODO: strings
+
+# s = '"we are learning"'
+# s = "we are learning"
+# print(s)
+# print(type(s))
+
+# m = "let's start " \
+#  "playing"
+# print(m)
+# print(type(m))
+
+# msg = '''
+# It is a 
+# multiline 
+# string
+# '''
+
+# print(msg)
+
+# """ """
+
+# TODO: booleans (True/ False)
+
+# is_valid = True
+# print(is_valid)
+# print(type(is_valid))
+
+# is_admin = False
+# print(is_admin)
+# print(type(is_admin))
+
+
+# TODO: None
+# temp = None
+
+# print(temp)
+# print(type(temp))
+
+
+# TODO: Derived datatypes
+
+# List -> [ ]
+# marks = [19, 15, 14, 19, 16, 15]
+# print(marks)
+# print(type(marks))
+
+# tuple -> ( )
+# marks = (19, 15, 14, 19, 16, 15)
+# print(marks)
+# print(type(marks))
+
+# set -> { }
+# marks = {19, 15, 14, 19, 16, 15}
+# print(marks)
+# print(type(marks))
+
+# dictionary -> {key : value}
+
+# user = {
+#     "first_name" : "John",
+#     "second_name" : "doe",
+#     "age" : 15,
+#     "courses" : ["Maths" , "Sci"]
+# }
+
+# print(user)
+# print(type(user))
+
+# range 
+# range (start_index, end_index , step)
+# end_index is not included
+# start_index is included
+# step is optional
+
+# nums = list(range(1, 6))
+# nums = list(range(6))
+# print(nums)
+# print(type(nums))
+
+rev = list(range(10 , 0 , -1))
+print(rev)
+print(type(rev))
+
+# even = tuple(range(0 , 11 , 2))
+# print(even)
+
+# odd = list(range(1, 10 ,2))
+# print(odd)
+
+# multiple_of_5 = tuple(range(0, 51, 5))
+# print(multiple_of_5)
+
+
---
+++ 01basics/05type_conversion.py
@@ -0,0 +1,29 @@
+'''
+Type conversion:
+When we explicitly change the datatype
+
+int()
+float()
+bool()
+str()
+list()
+tuple()
+set()
+dict()
+'''
+
+# a =float(5)
+# a = str(5)
+
+# a  = '5'
+# a = 'fds'
+# b= int(a)
+# print(a , type(a))
+# print(b , type(b))
+
+# is_valid = int(True)
+# print(is_valid , type(is_valid))
+
+name = list("John")
+print(name)
+print(type(name))
