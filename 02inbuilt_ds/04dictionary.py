467920df1df201d0b4d9d1ceb83eb3b40a0ca2e4
akshita garg <akshitagarg275@gmail.com>
2022-10-05 09:00:32 +0530

    day 9: strings

--- 02inbuilt_ds/04dictionary.py
+++ 02inbuilt_ds/04dictionary.py
@@ -42,3 +42,35 @@ user = {
 # for k,v in user.items():
 #     print("key is : ",k)
 #     print("value is: ",v)
+
+
+user = {
+    "fname" : "John",
+    "lname" : "Doe",
+    "age" : 12,
+    "courses" : ["python" , "HTML"],
+    "isPaid" : True
+}
+
+# print(user)
+# print(user.pop("age"))
+# print(user.popitem())
+# print(user)
+
+# user.setdefault("age" , 15)
+# user.setdefault("gender" , "male")
+# print(user)
+
+# TODO: WAP to find freq of each char
+# HAHA -  {'H' : 2 , 'A' : 2}
+
+# input_str = "HAhA"
+# freq = {}
+# for s in input_str:
+#     if freq.get(s) == None :
+#         freq[s] = 1
+#     else:
+#         freq[s] = freq[s] + 1
+# print(freq)
+
+# para ="we are learning python"
---
+++ 02inbuilt_ds/05strings.py
@@ -0,0 +1,86 @@
+'''
+strings : '' ," " , ''' '''
+str()
+
+iterable
+ordered
+immutable
+'''
+
+s = 'python'
+
+# for i in s:
+#     print(i, end=" ")
+# print(s[0])
+
+# i = 0
+
+# while i < len(s):
+#     print(s[i])
+#     i = i + 1
+
+# TODO: will give TypeError
+# s[0] = "C"
+
+
+# TODO: slicing
+# print(s[ : : ])
+# print(s[ : : -1])
+# print(s[: : 2])
+# print(s[1: 4])
+# print(s[-4 : -1])
+
+s = 'python is funny'
+
+# print(s.upper())
+# print(s.lower())
+
+# print(s.startswith('py'))
+# print(s.endswith('py'))
+
+# print(s.capitalize())
+# print(s.title())
+
+# print(s.count('o'))
+
+# TODO: WAP to find no of vowels 
+
+# print(s.find('t'))
+# print(s.find('y'))
+# print(s.find('you'))
+
+# print(s.index("you"))
+
+# s = 'python12'
+# print(s.isalpha())
+# print(s.isalnum())
+
+# contact = '12142s'
+# print(contact.isdigit())
+
+# password = "       "
+# print(password.isspace())
+
+# print(s.isidentifier())
+# print('for'.isidentifier())
+
+
+'''
+\b -> backspace
+\t -> tab
+\\ -> \
+\' ->'
+\-> new line
+'''
+# path =r"C:\navin\tmp"
+# print(path)
+
+# TODO: format strings
+
+# TODO: split()
+
+# TODO: replace
+
+# TODO: join
+
+# TODO: trim()
