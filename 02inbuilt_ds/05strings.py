4eb6e3cd46bab822d5c54ab124e937298b5f869d
akshita garg <akshitagarg275@gmail.com>
2022-10-06 08:49:48 +0530

    day 10: strings

--- 02inbuilt_ds/05strings.py
+++ 02inbuilt_ds/05strings.py
@@ -77,10 +77,77 @@ s = 'python is funny'
 
 # TODO: format strings
 
-# TODO: split()
+n1 = 2
+n2 = 3
+ans = 5
+#  2 + 3 = 5
+# print(str(n1) + " + " + str(n2) + " = " + str(ans))
+# print("{1} + {0} = {2}".format(n1,n2,ans))
 
-# TODO: replace
+# f-strings
+# print(f"{n1} + {n2} = {ans}")
+
+
+# TODO: split(delimiter)
+
+s = "we are learning pthon"
+# print(s.split())
+# print(s.split('e'))
+# email = 'john.doe@abc.com'
+# print(email.split('@')[0])
+# print(email.split('@')[1])
+
+# nums = input("enter the nums").split()
+# print(nums)
+
+# TODO: replace(old_string, new_string, count)
+# s = "we are learning pthon"
+
+# print(s.replace('a' , '@' , 1))
+
 
 # TODO: join
 
-# TODO: trim()
+# fname = "john"
+# lname = "doe"
+
+# uname = '.'.join([fname, lname])
+# print(uname)
+
+# company = "abc.com"
+
+# email = '@'.join([uname, company])
+# print(email)
+# TODO: strip()
+
+# password = "      abc123     "
+# print(password.lstrip())
+# print(password.rstrip())
+# print(password.strip())
+
+
+# TODO : WAP to remove all the spaces from the given string
+s = "we are learning python"
+# print(s.replace(" ", ""))
+# for i in range(len(s)):
+#     if s[i] == " ":
+#         s.replace(s[i] , "" )
+
+
+# TODO: WAP to reverse a string
+# rev = ""
+# for i in s:
+#     rev = i + rev
+# print(rev)
+
+# print(s[: : -1])
+
+
+# TODO: reverse each word
+# s = "we are learning python"
+# "ew rea gninrael nohtyp"
+# s_list = s.split()
+# for i in range(len(s_list)):
+#     s_list[i] = s_list[i][: : -1]
+# rev_words = " ".join(s_list)
+# print(rev_words)
---
+++ 02inbuilt_ds/ques.py
@@ -0,0 +1,10 @@
+'''
+# 1:A:0, 2:B:1, 3:C:1, 4:D:2, 5:E:2, 6:F:3 - input string
+# Output:
+# A - B,C
+# B - D,E
+# C - F
+# D -
+# E -
+# F -
+'''
